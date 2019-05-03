0f1e937ddbcbd01b27710d0888128a7607a16d56
kknickkk <ng.durante@gmail.com>
2019-05-03 19:21:53 +0200

    RX window management

--- quickmode/rx_very_simple.py
+++ quickmode/rx_very_simple.py
@@ -56,76 +56,76 @@ data_size = 31
 loop = True
 
 while loop:
-
-    # Set window variables
-    rx_id = []      # The receiver will check this after receiving a window. Example: [0, 1, 2, 4, 6] --> I have to ask for retx of pkt 3 and 5
-    window_bytes = [0] * WINDOW_SIZE * data_size
-    last_packet = False
-    ask_for_rtx = False
-    end_of_window = False
-    last_window = 32
-
-    while(not end_of_window):
-
-        while(radio_rx.available()):
-
-            # First check of the payload
-            length = radio_rx.getDynamicPayloadSize()
-            receive_payload = radio_rx.read(length)
-            # now process rx_payload
-            header = receive_payload[0]
-            if(header > 127):
-                # This means that eot = 1, the header field will be something like = 1XXX XXXX so it will be > 127
-                last_packet = True
-                frame_id = 0x7f & header
-                last_window = int(frame_id) % WINDOW_SIZE
-                # frames.append(receive_payload[1:])
-            else:
-                # hex 7f is 127 i.e. 1111111
-                frame_id = 0x7f & header
-                window_id = int(frame_id) % WINDOW_SIZE
-                print("Received packet id: " + str(frame_id))
-
-                if(window_id not in rx_id):
-                    rx_id.append(window_id)
-                window_bytes[window_id:window_id + data_size] = receive_payload[1:]
-
-            if((len(rx_id) == WINDOW_SIZE) or (len(rx_id) == int(last_window) + 1)):
-                end_of_window = True
-
-
-        # send correct ids (rx_id)
-        rx_id.sort()
-        if(rx_id[-1] == WINDOW_SIZE - 1):
-            print("Sending ACK: " + str(rx_id))
-            radio_tx.send(bytes(rx_id))
-
-    # Once all the window is received correctly, store the packets
-    frames.append(window_bytes)
-    print("End of window, packet saved")
-    # If it is the last packet save the txt
-
-
-    if last_packet:
-        # led.green()
-        print('Reception complete.')
-        # If we are here it means we received all the frames so we have to uncompress
-        print("Type of the list")
-        print(type(frames))
-        print("Type of one element")
-        print(type(frames[0]))
-        uncompressed_frames = zlib.decompress(bytes(frames))
-        f = open('file' + str(num_file) + '.txt', 'wb')
-        f.write(bytes(uncompressed_frames))
-        f.close()
-        print('File saved')
-        frames = []
+    if(rx.available()):
+        # Set window variables
+        rx_id = []      # The receiver will check this after receiving a window. Example: [0, 1, 2, 4, 6] --> I have to ask for retx of pkt 3 and 5
+        window_bytes = [0] * WINDOW_SIZE * data_size
         last_packet = False
-        num_packets = 0
-        num_file += 1
-        input('Press Enter to finish')
-        led.off()
-        loop = 0
+        ask_for_rtx = False
+        end_of_window = False
+        last_window = 32
+
+        while(not end_of_window):
+
+            while(radio_rx.available()):
+
+                # First check of the payload
+                length = radio_rx.getDynamicPayloadSize()
+                receive_payload = radio_rx.read(length)
+                # now process rx_payload
+                header = receive_payload[0]
+                if(header > 127):
+                    # This means that eot = 1, the header field will be something like = 1XXX XXXX so it will be > 127
+                    last_packet = True
+                    frame_id = 0x7f & header
+                    last_window = int(frame_id) % WINDOW_SIZE
+                    # frames.append(receive_payload[1:])
+                else:
+                    # hex 7f is 127 i.e. 1111111
+                    frame_id = 0x7f & header
+                    window_id = int(frame_id) % WINDOW_SIZE
+                    print("Received packet id: " + str(frame_id))
+
+                    if(window_id not in rx_id):
+                        rx_id.append(window_id)
+                    window_bytes[window_id:window_id + data_size] = receive_payload[1:]
+
+                if((len(rx_id) == WINDOW_SIZE) or (len(rx_id) == int(last_window) + 1)):
+                    end_of_window = True
+
+
+            # send correct ids (rx_id)
+            rx_id.sort()
+            if(len(rx_id) > 0 and rx_id[-1] == WINDOW_SIZE - 1):
+                print("Sending ACK: " + str(rx_id))
+                radio_tx.send(bytes(rx_id))
+
+        # Once all the window is received correctly, store the packets
+        frames.append(window_bytes)
+        print("End of window, packet saved")
+        # If it is the last packet save the txt
+
+
+        if last_packet:
+            # led.green()
+            print('Reception complete.')
+            # If we are here it means we received all the frames so we have to uncompress
+            print("Type of the list")
+            print(type(frames))
+            print("Type of one element")
+            print(type(frames[0]))
+            uncompressed_frames = zlib.decompress(bytes(frames))
+            f = open('file' + str(num_file) + '.txt', 'wb')
+            f.write(bytes(uncompressed_frames))
+            f.close()
+            print('File saved')
+            frames = []
+            last_packet = False
+            num_packets = 0
+            num_file += 1
+            input('Press Enter to finish')
+            led.off()
+            loop = 0
 
 
 
