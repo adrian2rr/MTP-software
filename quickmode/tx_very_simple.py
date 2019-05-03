3a88e2c7f68accbbf2ba9af95faf972c3578e5e4
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2019-05-03 20:44:53 +0200

    Restructured code

--- quickmode/tx_very_simple.py
+++ quickmode/tx_very_simple.py
@@ -48,63 +48,53 @@ tot_packets = len(packets)
 window_counter = 0
 WINDOW_SIZE = 32 # TODO: Put in config file
 
-efficient = False
 # TODO: to avoid this code mess (below), make classes transmitter and receiver, these ones will have methods like: stop_and_wait(already implemented), sliding_window (this one)
-if(not efficient):
-    for window_counter in range(tot_packets//WINDOW_SIZE):
-        # rx_acks => remaining packets to send
-        t = time.time()
-        rx_acks = 0 # esto de crear la lista asi y aqui no me gusta, habra que cambiarlo, por un contador? --> efficient version
-        rx_acks_bools = [0] * WINDOW_SIZE # 0 if the receiver has not received the packet, 1 if the receiver has sent ACK
-        elapsed = time.time() - t
-        print("Elapsed time = " + str(elapsed))
+
+for window_counter in range(tot_packets//WINDOW_SIZE):
+    # rx_acks => remaining packets to send
+    t = time.time()
+    rx_acks = 0 # esto de crear la lista asi y aqui no me gusta, habra que cambiarlo, por un contador? --> efficient version
+    rx_acks_bools = [0] * WINDOW_SIZE # 0 if the receiver has not received the packet, 1 if the receiver has sent ACK
+    elapsed = time.time() - t
+    print("Elapsed time = " + str(elapsed))
+    timeout = False
+    # si ha saltado el timeout o el numbero de acks recibidos es menor que la window size tendra que enviar 
+    while( (rx_acks < WINDOW_SIZE) or (timeout) ): 
         timeout = False
-        # si ha saltado el timeout o el numbero de acks recibidos es menor que la window size tendra que enviar 
-        while( (rx_acks < WINDOW_SIZE) or (timeout) ): 
-            timeout = False
-            for packet_index in range(len(rx_acks_bools)):
-                if(rx_acks_bools[packet_index] == 0):
-                    radio_tx.write(packets[window_counter * WINDOW_SIZE + packet_index])
-                    print("tx packet " + str(window_counter*WINDOW_SIZE+packet_index))
-            # Once it has sent all the packets in the window it checks the ACK and checks the timeout
-            started_waiting_at = millis()
-            while (not radio_rx.available()) and (not timeout):
-                if (millis() - started_waiting_at) > int(config.timeout_time):
-                    timeout = True
-                    started_waiting_at = millis()
-                    
-            if(timeout):
-                print("Ha saltado el timeout")
-            else:
-                # There is sth in the receiver
-                print("I listen to ACK")
-                ack = radio_rx.read(radio_rx.getDynamicPayloadSize())
-                # if(len(ack) == WINDOW_SIZE):
-                #     # All packets are OK
-                #     print("ALL PACKETS OK!!!!!!")
-                # else:
-                # Some packets are wrong, they will send the ones that are good
+        for packet_index in range(len(rx_acks_bools)):
+            if(rx_acks_bools[packet_index] == 0):
+                radio_tx.write(packets[window_counter * WINDOW_SIZE + packet_index])
+                print("tx packet " + str(window_counter*WINDOW_SIZE+packet_index))
+        # Once it has sent all the packets in the window it checks the ACK and checks the timeout
+        started_waiting_at = millis()
+        while (not radio_rx.available()) and (not timeout):
+            if (millis() - started_waiting_at) > int(config.timeout_time):
+                timeout = True
+                started_waiting_at = millis()
                 
-                for ack_idx in ack:
-                    if(rx_acks_bools[ack_idx] == 0):
-                        print("Some packets are wrong")
-                        rx_acks += 1
-                        rx_acks_bools[ack_idx] = 1
-                print("ACKS array")
-                print(rx_acks_bools)
-                print("Number of acks " +str(rx_acks))
+        if(timeout):
+            print("Ha saltado el timeout")
+        else:
+            # There is sth in the receiver
+            print("I listen to ACK")
+            ack = radio_rx.read(radio_rx.getDynamicPayloadSize())
+            # if(len(ack) == WINDOW_SIZE):
+            #     # All packets are OK
+            #     print("ALL PACKETS OK!!!!!!")
+            # else:
+            # Some packets are wrong, they will send the ones that are good
+            
+            for ack_idx in ack:
+                if(rx_acks_bools[ack_idx] == 0):
+                    print("Some packets are wrong")
+                    rx_acks += 1
+                    rx_acks_bools[ack_idx] = 1
+            print("ACKS array")
+            print(rx_acks_bools)
+            print("Number of acks " + str(rx_acks))
 
         
-else: 
-    for window_counter in range(tot_packets//WINDOW_SIZE):
-        retransmit = False # Se pone a True si hay que retransmitir, ya sea por timeout o por que se lo pide el receptor
-        timeout = False # Se pone a True si salta el timeout
-        started_waiting_at = millis()
 
-        while(not retransmit):
-            for packet_index in range(WINDOW_SIZE):
-                radio_tx.write(packets[window_counter * WINDOW_SIZE + packet_index])
-            
             
             
 
