6cec9288e374f834a57321e1e9014d23811d639e
kknickkk <ng.durante@gmail.com>
2019-05-03 18:27:20 +0200

    RX window management

--- quickmode/rx_very_simple.py
+++ quickmode/rx_very_simple.py
@@ -35,7 +35,7 @@ channel_RX = 60
 channel_TX = 70
 
 # Initialize radio
-radio_tx, radio_rx = configure_radios(channel_TX, channel_RX,0)
+radio_tx, radio_rx = configure_radios(channel_TX, channel_RX, 0)
 radio_rx.startListening()
 radio_tx.stopListening()
 
@@ -58,55 +58,68 @@ loop = True
 while loop:
 
     # Set window variables
-    rx_id  = [] # The receiver will check this after receiving a window. Example: [0, 1, 2, 4, 6] --> I have to as for retx of pkt 3 and 5
+    rx_id = []      # The receiver will check this after receiving a window. Example: [0, 1, 2, 4, 6] --> I have to ask for retx of pkt 3 and 5
     window_bytes = [0] * WINDOW_SIZE * data_size
     last_packet = False
     ask_for_rtx = False
+    end_of_window = False
+    last_window = 32
 
-    while(radio_rx.available()):
-        #First check of the payload
-        len = radio_rx.getDynamicPayloadSize()
-        receive_payload = radio_rx.read(radio_rx.getDynamicPayloadSize())
-        # now process rx_payload
-        header = receive_payload[0]
-        if(header > 127):
-            # This means that eot = 1, the header field will be something like = 1XXX XXXX so it will be > 127
-            last_packet = True
-            frames.append(receive_payload[1:])
-        else:
-            frame_id = 0x7f & header
-            rx_id.append(frame_id)
-            window_bytes[frame_id:frame_id+data_size] = receive_payload[1:] 
+    while(not end_of_window):
 
-        if(len(rx_id) < WINDOW_SIZE):
+        while(radio_rx.available()):
 
-        # Once all the window is received correctly, store the packets
-        frames.append(window_bytes)
-        
-        break
-        
-        # If it is the last packet save the txt
-        if last_packet:
-            #led.green()
-            print('Reception complete.')
-            # If we are here it means we received all the frames so we have to uncompress
-            print("Type of the list")
-            print(type(frames))
-            print("Type of one element")
-            print(type(frames[0]))
-            uncompressed_frames = zlib.decompress(bytes(frames))
-            f = open('file'+str(num_file)+'.txt','wb')
-            f.write(bytes(uncompressed_frames))
-            f.close()
-            print('File saved')
-            frames = []
-            last_packet = False
-            num_packets = 0
-            num_file += 1
-            input('Press Enter to finish')
-            led.off()
-            loop = 0
+            # First check of the payload
+            len = radio_rx.getDynamicPayloadSize()
+            receive_payload = radio_rx.read(len)
+            # now process rx_payload
+            header = receive_payload[0]
+            if(header > 127):
+                # This means that eot = 1, the header field will be something like = 1XXX XXXX so it will be > 127
+                last_packet = True
+                frame_id = 0x7f & header
+                last_window = frame_id % WINDOW_SIZE
+                # frames.append(receive_payload[1:])
+            else:
+                # hex 7f is 127 i.e. 1111111
+                frame_id = 0x7f & header
+                window_id = frame_id % WINDOW_SIZE
+                rx_id.append(window_id)
+                window_bytes[window_id:window_id + data_size] = receive_payload[1:]
 
+            if(len(rx_id) == WINDOW_SIZE or len(rx_id) == last_window + 1):
+                end_of_window = True
+
+        # send correct ids (rx_id)
+        if(rx_id[-1] == WINDOW_SIZE - 1):
+            radio_tx.send(bytes(rx_id))
+
+    # Once all the window is received correctly, store the packets
+    frames.append(window_bytes)
+    # If it is the last packet save the txt
+
+
+
+    if last_packet:
+        #led.green()
+        print('Reception complete.')
+        # If we are here it means we received all the frames so we have to uncompress
+        print("Type of the list")
+        print(type(frames))
+        print("Type of one element")
+        print(type(frames[0]))
+        uncompressed_frames = zlib.decompress(bytes(frames))
+        f = open('file'+str(num_file)+'.txt','wb')
+        f.write(bytes(uncompressed_frames))
+        f.close()
+        print('File saved')
+        frames = []
+        last_packet = False
+        num_packets = 0
+        num_file += 1
+        input('Press Enter to finish')
+        led.off()
+        loop = 0
 
 
 
--- quickmode/tx_very_simple.py
+++ quickmode/tx_very_simple.py
@@ -31,7 +31,7 @@ channel_RX = 70
 payload_size = config.payload_size
 
 # Start radios
-radio_tx, radio_rx = configure_radios(channel_TX, channel_RX,1)
+radio_tx, radio_rx = configure_radios(channel_TX, channel_RX, 1)
 radio_rx.startListening()
 radio_tx.stopListening()
 
@@ -39,62 +39,56 @@ radio_tx.stopListening()
 packet_manager = PacketManager(config.document_path)
 packets = packet_manager.create_window()
 
-#Define loop variables
+# Define loop variables
 led.violet()
-i=0
+i = 0
 
 # We'll have tot_packets/WINDOW_SIZE windows sent, in each window we'll send WINDOW_SIZE packets
 tot_packets = len(packets)
 window_counter = 0
-WINDOW_SIZE = 32 # TODO: Put in config file
+WINDOW_SIZE = 32 	# TODO: Put in config file
 ACK_SIZE = 32
 
 efficient = False
 # TODO: to avoid this code mess (below), make classes transmitter and receiver, these ones will have methods like: stop_and_wait(already implemented), sliding_window (this one)
 if(not efficient):
-    for window_counter in range(tot_packets//WINDOW_SIZE):    
+    for window_counter in range(tot_packets // WINDOW_SIZE):
         # rx_acks => remaining packets to send
-        rx_acks = [ i for i in range(WINDOW_SIZE)] # esto de crear la lista asi y aqui no me gusta, habra que cambiarlo, por un contador? --> efficient version
+        rx_acks = [i for i in range(WINDOW_SIZE)] 	# esto de crear la lista asi y aqui no me gusta, habra que cambiarlo, por un contador? --> efficient version
         timeout = False
         # Start timeout
         started_waiting_at = millis()
-        # si ha saltado el timeout o el numbero de acks recibidos es menor que la window size tendra que enviar 
-        while( (len(rx_acks) > 0) or (not timeout) ): 
+        # si ha saltado el timeout o el numbero de acks recibidos es menor que la window size tendra que enviar
+        while((len(rx_acks) > 0) or (not timeout)):
             for packet_index in rx_acks:
                 radio_tx.write(packets[window_counter * WINDOW_SIZE + packet_index])
             # Once it has sent all the packets in the window it cheks the ACK and checks the timeout
             while (not radio_rx.available()) and (not timeout):
                 if (millis() - started_waiting_at) > int(config.timeout_time):
                     timeout = True
-            if(timeout): 
+
+            if(timeout):
                 timeout = False
             else:
                 # There is sth in the receiver
                 ack = radio_rx.read(ACK_SIZE)
-              
-                if(ack[0]=255):
+
+                if(ack[0] == 255):
                     # All packets are OK, go to the next window
                     rx_acks = []
                 else:
                     for ack_idx in range(ACK_SIZE):
                         del rx_acks[ack_idx]
 
-else: 
-    for window_counter in range(tot_packets//WINDOW_SIZE):
-        retransmit = False # Se pone a True si hay que retransmitir, ya sea por timeout o por que se lo pide el receptor
-        timeout = False # Se pone a True si salta el timeout
+        window_counter += 1
+else:
+    for window_counter in range(tot_packets // WINDOW_SIZE):
+        retransmit = False		# Se pone a True si hay que retransmitir, ya sea por timeout o por que se lo pide el receptor
+        timeout = False 		# Se pone a True si salta el timeout
         started_waiting_at = millis()
 
         while(not retransmit):
             for packet_index in range(WINDOW_SIZE):
                 radio_tx.write(packets[window_counter * WINDOW_SIZE + packet_index])
-            
-            
-            
-
-
-
-
-
 
 led.off()
