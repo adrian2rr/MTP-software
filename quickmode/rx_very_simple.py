7ee27303f8fa4d0759dcf228cb747f8b7e5bc0dd
kknickkk <ng.durante@gmail.com>
2019-05-05 21:01:26 +0200

    Compression

--- quickmode/rx_very_simple.py
+++ quickmode/rx_very_simple.py
@@ -141,7 +141,12 @@ while loop:
             print(type(frames))
             print("Type of one element")
             print(type(frames[0]))
-            uncompressed_frames = zlib.decompress(frames)
+            frame_bytes = []
+            for item in frames:
+                frame_bytes += str(item)
+            frame_bytes = bytes([int(frame_bytes)])
+
+            uncompressed_frames = zlib.decompress(frame_bytes)
             f = open('file' + str(num_file) + '.txt', 'wb')
             f.write(bytes(uncompressed_frames))
             f.close()
