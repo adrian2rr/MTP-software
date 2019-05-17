d54906175053e13f56c8593023ca3f01a3fbcead
Josep Torné Chertó <lltorne95ll@gmail.com>
2019-05-17 09:38:45 +0200

    Changed transceiver pins

--- window/utils/radio.py
+++ window/utils/radio.py
@@ -6,12 +6,12 @@ def configure_radios(channel_TX, channel_RX, function, rate = 1):
 
     # Setup CE and CSN pin with SPI velocity and define pipe adresses depending if it acts as a transmitter or as a receiver
     if (function==1):
-        radio_rx = RF24(RPI_V2_GPIO_P1_13, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
-        radio_tx = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS1, BCM2835_SPI_SPEED_8MHZ)
+        radio_tx = RF24(RPI_V2_GPIO_P1_13, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
+        radio_rx = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS1, BCM2835_SPI_SPEED_8MHZ)
         pipes = [0xf0f0f0f0e1, 0xf0f0f0f0d2]
     else:
-        radio_tx = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS1, BCM2835_SPI_SPEED_8MHZ)
-        radio_rx = RF24(RPI_V2_GPIO_P1_13, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
+        radio_rx = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS1, BCM2835_SPI_SPEED_8MHZ)
+        radio_tx = RF24(RPI_V2_GPIO_P1_13, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
         pipes = [0xf0f0f0f0d2, 0xf0f0f0f0e1]
     
     # Initialize the transceivers
